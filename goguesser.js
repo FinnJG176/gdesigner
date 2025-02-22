98fc3ac96e8c909e131d627717843c36d5196fc2
Finnegan Granholm <finnegangranholm@Macbook-Air-M2.local>
2025-02-22 08:02:47 -0500

    Major updater 01.2

---
+++ goguesser.js
@@ -0,0 +1,7 @@
+const guessInput =  document.getElementById("guessInput").addEventListener("click");
+
+document.getElementById(id).onclick = function(){code}
+
+element.setAttribute(guessInput, value)
+
+Math.floor(Math.random() * 100) + 1
