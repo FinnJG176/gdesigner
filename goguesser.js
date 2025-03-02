155cfabc8d685111211dd01f545064410c15a0b4
Finnegan Granholm <finnegangranholm@Macbook-Air-M2.local>
2025-03-02 12:21:48 -0500

    GoGuesser complete

--- goguesser.js
+++ goguesser.js
@@ -1,7 +1,24 @@
-const guessInput =  document.getElementById("guessInput").addEventListener("click");
+let randomNumber = Math.floor(Math.random() * 100) + 1
 
-document.getElementById(id).onclick = function(){code}
+const guessInput = document.getElementById("guessInput");
+const guessButton = document.getElementById("guessButton")
+const feedback = document.getElementById("feedback")
 
-element.setAttribute(guessInput, value)
+guessButton.addEventListener("click", function () {
+    let userGuess = Number(guessInput.value);
+    console.log("userGuess, randomnumber", userGuess, randomNumber)
 
-Math.floor(Math.random() * 100) + 1
+    if (userGuess > 100 || guessInput < 0) {
+        feedback.textContent = "Out of range! Make sure it's between 1 and 100."
+        feedback.style.color = "red";
+    } else if (userGuess > randomNumber) {
+        feedback.textContent = "Too high!";
+        feedback.style.color = "red";
+    } else if (userGuess < randomNumber) {
+        feedback.textContent = "Too low!"
+        feedback.style.color = "red";
+    } else {
+        feedback.textContent = "Exactly right! Refresh the page to play again."
+        feedback.style.color = "green";
+    }
+});
