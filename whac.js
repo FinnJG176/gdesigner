f2df0a210abcbe7987cacbe9cb1c641545b80b81
Finnegan Granholm <finnegangranholm@Macbook-Air-M2.local>
2025-04-25 16:37:23 -0400

    whac mole i78

--- whac.js
+++ whac.js
@@ -1,13 +1,33 @@
 const themeSelector = document.getElementById("theme");
 const holes = document.querySelectorAll('.hole');
 const body = document.body;
+const start = document.getElementById("start");
+const timeText = document.getElementById("time");
+let timer;
+let moleTimer;
+let currentTime = 30;
+let score = 0;
+function startGame() {
+    currentTime = 30;
+    timeText.textContent = currentTime;
+    
+    clearInterval(timer);
+    clearInterval(moleTimer);
 
-themeSelector.addEventListener('change', (event)=> {
-    const selectedTheme = event.target.value;
+    timer = setInterval(countDown, 1000);
+    moleTimer = setInterval(showMole, 1500);
+}
 
-    body.classList.remove('easterspecial', 'hp', 'skywalker');
-    body.classList.add(selectedTheme);
-});
+function countDown() {
+    currentTime--;
+    timeText.textContent = currentTime;
+    if (currentTime === 0 ){
+        clearInterval(timer);
+        clearInterval(moleTimer);
+        // add prnt score
+    }
+
+}
 
 function showMole() {
     // randomhole 
@@ -27,3 +47,29 @@ function showMole() {
         }, 1000)
     }
 }
+// go through each hole
+holes.forEach(hole => {
+    hole.addEventListener('click', () => {
+    // check if the hole clicked on has show in the classlist
+    // this means theres a mole in the hole
+        if (hole.classList.contains('show')) {
+            score ++ ;
+            // update score text
+            hole.classList.remove("show");
+        }
+    })
+})
+
+// hw finish themes & score update onscreen
+// opt. hammer/sound effects
+
+// if so clear the mole and increase the score
+
+themeSelector.addEventListener('change', (event)=> {
+    const selectedTheme = event.target.value;
+
+    body.classList.remove('lordmole','easterspecial', 'hp', 'skywalker','pixel' );
+    body.classList.add(selectedTheme);
+});
+
+start.addEventListener('click', startGame)
